9863e23ba42c973cd91bec45c27b8ddeda6888e5
peelarajkumar <peelarajkumar@gmail.com>
2025-04-01 11:46:12 +0530

    changed Services style

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -9,15 +9,13 @@ const Services = () => {
       <h4 className='text-center mb-2 text-lg font-Ovo'>What i offer</h4>
       <h2 className='text-center mb-2 text-5xl font-Ovo'>My Services</h2>
       </div>
-      <div className='grid grid-cols-4 gap-5 px-[8%]'>
+      <div className='grid grid-cols-2 sm:grid-cols-4 gap-6 mb-3 px-[8%]'>
        {serviceData.map(({icon,title,description,link},index)=>(
         <div  key={index}>
           <Image alt='services Logo' className='w-10' src={icon}/>
           <h3 className='text-lg my-4 text-black-700 '>{title}</h3>
           <p className='text-sm text-gray-800 leading-5'>{description}</p>
         </div>))}
-
-
       </div>
     </div>
   )
